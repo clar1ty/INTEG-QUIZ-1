b9f5e356226bb3c382c8e43c4018a408299c05fe
clarity <clarkjames0028@gmail.com>
2021-07-17 18:39:30 +0800

    moved feature logic to bl layer

--- UI/Program.cs
+++ UI/Program.cs
@@ -1,130 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using DL;
+using BL;
+
 
 namespace UI
 {
     class Program
     {
-
-        static List<string> showoptions = new List<string>()
-                    {
-                        "(1) Show me courier list.",
-                        "(2) Lemme create an account.",
-                        "(3) Lemme delete  a nub account.",
-                        "(4) NVM, just exit."
-                    };
-        static void Main(string[] args)
+        static void Main(String[] args)
         {
-            
-            ProcessActionsForAdmin();
-        }
-
-        static void ProcessActionsForAdmin()
-        {
-            for (int adminchoice = GetUserChoice();
-                adminchoice != 4;
-                adminchoice = GetUserChoice())
-            {
-                switch (adminchoice)
-                {
-                    case 1:
-                        Console.Clear();
-                        SqlData.DisplayCourierData();
-                        break;
-                    case 2:
-                        Console.Clear();
-                        CreateNewAccount();
-                        break;
-                    case 3:
-                        Console.Clear();
-                        DeleteAnAccount();
-                        break;
-                    default:
-                        Console.Clear();
-                        Console.WriteLine("What are you doing!? Try again bruh!");
-                        break;
-                }
-                Console.WriteLine();
-            }
-            Console.Clear();
-            Console.WriteLine("Application exiting...");
-            Console.ReadKey();
-        }
-
-        static int GetUserChoice()
-        {
-            try
-            {
-                ShowOptions();
-                Console.Write("Choice: ");
-                return Convert.ToInt32(Console.ReadLine());
-            }
-            catch
-            {
-                Console.Clear();
-                Console.WriteLine("Error occured. Please try again");
-                return GetUserChoice();
-            }
-        }
-
-        static void ShowOptions()
-        {
-            Console.WriteLine("Sup admin, wachu wanna do now?\n");
-
-            foreach (var option in showoptions)
-            {
-                Console.WriteLine(option);
-            }
-        }
-
-        static void CreateNewAccount()
-        {
-            string name, id; int contact;
-            Console.WriteLine("Yo, give me the following details:\n");
-
-            Console.Write("Name: ");
-            name = Console.ReadLine();
-
-            Console.Write("ID: ");
-            id = Console.ReadLine();
-
-            Console.Write("Contact Number: ");
-            contact = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("\nAccount successfully created dude!");
-            Console.ReadKey();
-            Console.Clear();
-
-            SqlData.CreateAnAccount(name, id, contact);
-        }
-
-        static void DeleteAnAccount()
-        {
-            SqlData.DisplayCourierData();
-
-
-            Console.WriteLine("Yo, gimme the name of this nub u wanna DELETE!\n");
-            Console.Write("Name: ");
-            string name = Convert.ToString(Console.ReadLine());
-
-            var VerifyIfAccountExist = SqlData.ValidateAccount(name);
-
-            if (VerifyIfAccountExist.Equals("True"))
-            {
-                Console.WriteLine("Congrats! Nub deleted!");
-                Console.ReadKey();
-                Console.Clear();
-            }
-            else
-            {
-                Console.WriteLine("Error nub! Try again!");
-                Console.ReadKey();
-                Console.Clear();
-                DeleteAnAccount();
-            }
-
-            SqlData.DeleteAnAccount(name);
+            CMSLogic.ProcessActionsForAdmin();
         }
     }
 }
